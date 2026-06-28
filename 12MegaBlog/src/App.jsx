8c79bdc7a67724248fd8738edae7f11129f56a06
vikashgoswami30 <109417405+vikashgoswami30@users.noreply.github.com>
2026-06-28 23:18:39 +0530

    done for the day

---
+++ 12MegaBlog/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import conf from './conf/conf.js'
+function App() {
+  const appwriteUrl = conf.appwriteUrl
+ console.log(appwriteUrl)
+  return (
+    <>
+    <h1>Vikash</h1>
+    </>
+  )
+}
+
+export default App
